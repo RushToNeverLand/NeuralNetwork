abae84a512bb5613f36581d091069c8e1d5118bc
RushToNeverLand <chenyue0210@outlook.com>
2020-11-28 18:26:05 +0800

    add forward (matrix multiply)

--- Final_1/network.py
+++ Final_1/network.py
@@ -3,37 +3,36 @@ from util import sigmoid, inv_sigmoid
 
 
 class Network:
-    def __init__(self, input_dim, hidden_dim, output_dim, act_type='sigmoid', lr=0.01):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_hid_layer=1, act_type='relu', lr=0.01):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
+        self.num_hid_layer = num_hid_layer
+
         self.act_type = act_type
         self.lr = lr
 
+        # FIX: dynamic add layer
         # [input_dim, output_dim]
         self.l1 = np.random.normal(size=(input_dim, hidden_dim))
         # [hidden_dim, hidden_dim]
         self.l2 = np.random.normal(size=(hidden_dim, hidden_dim))
         # [hidden_dim, output_dim]
-        self.l3 = np.random.normal(size=(output_dim, hidden_dim))
+        self.l3 = np.random.normal(size=(hidden_dim, output_dim))
         
-    def forward(self, x):
-        y1 = self.compute_layer(x, self.l1, self.input_dim, self.hidden_dim)
-        print(y1)
+    def predict(self, X):
+        y1 = self.compute_layer(X, self.l1)
         y1 = self.compute_activation(y1)
-        print(y1)
-        y2 = self.compute_layer(y1, self.l2, self.hidden_dim, self.hidden_dim)
+
+        y2 = self.compute_layer(y1, self.l2)
         y2 = self.compute_activation(y2)
-        y3 = self.compute_layer(y2, self.l3, self.hidden_dim, self.output_dim)
-        # y3 = self.compute_activation(y3)
+        
+        y3 = self.compute_layer(y2, self.l3)
         
         return y3
 
-    def compute_layer(self, x, layer, input_dim, output_dim):
-        output = np.zeros(input_dim)
-        for i in range(output_dim):
-            output[i] = layer[i] * x
-        return output
+    def compute_layer(self, data, Weight):
+        return np.matmul(data, Weight)
 
     def compute_activation(self, x):
         if self.act_type == 'sigmoid':
@@ -43,7 +42,9 @@ class Network:
         else:
             raise NotImplementedError
 
-    def backward(self, y_predict, y):
-        error = 0.5 * np.sum((y_predict-y)**2)
+    def fit(X, Y):
+        pred_Y = self.predict(X)
 
-
+        error = np.sum((pred_Y-Y)**2)
+        
+        
